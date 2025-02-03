9bdb5636f55c142dfb4fd9f21a49f300246f17a8
discountcharacter <125435003+discountcharacter@users.noreply.github.com>
2025-02-03 18:11:57 +0530

    Update ocr.js

--- backend/utils/ocr.js
+++ backend/utils/ocr.js
@@ -1,8 +1,8 @@
-const Tesseract = require('tesseract.js');
+import Tesseract from 'tesseract.js';
 
 const processImage = async (imagePath) => {
   const { data: { text } } = await Tesseract.recognize(imagePath, 'eng');
   return text;
 };
 
-module.exports = { processImage };
+export { processImage };
